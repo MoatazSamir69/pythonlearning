3eae8764277fc255bd920bd1c2a897836c185c04
MoatazSamir69 <motaz.samir935@gmail.com>
2022-09-24 06:55:40 +0200

    Multi Assaigment Section in Python Learning

---
+++ Mult Ass .py
@@ -0,0 +1,14 @@
+name = "Bro"
+age = 21
+attractive = True
+name , age , attractive = "Bro" , 21 , True
+print(name)
+print(age)
+print(attractive)
+
+spongebob = patrick = sandy = squidward = 30
+print(spongebob)
+print(patrick)
+print(sandy)
+print(squidward)
+ 
