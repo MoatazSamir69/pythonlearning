681beec1de2efd428d3c8c5fab356a8024391b69
MoatazSamir69 <motaz.samir935@gmail.com>
2022-09-24 06:26:13 +0200

    Variable Section in Python Learning

---
+++ Variable test .py
@@ -0,0 +1,24 @@
+name = "Bro"
+print(name)
+print("Hello " + name)
+print(type (name))
+
+first_name = "Bro"
+last_name = "Code"
+full_name = first_name + last_name
+print("Hello " + full_name)
+
+age = 21
+age += 1
+print(age)
+print(type(age))
+print("Your age is : " + str(age))
+
+height = 250.5
+print("Your height is ; " +  str(height) + "cm")
+print(type(height))
+
+human = False
+print("Are you a human: " + str(human))
+print(type(human))
+
