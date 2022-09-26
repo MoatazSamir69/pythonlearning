ce2abb84aa8556da668750d3d6d7dc665010f057
MoatazSamir69 <motaz.samir935@gmail.com>
2022-09-26 09:34:15 +0200

    For Loop Section in Python Learning

---
+++ For Lopp .py
@@ -0,0 +1,17 @@
+from time import time
+
+
+for i in range(10+1):
+    print(i+1)
+
+for j in range(50,100+1,5):
+    print(j)
+
+for k in "zeby":
+    print(k)    
+
+for second in range(10,0,-1):
+    print(second) 
+    time.sleep(1)
+print("HBD")    
+   
