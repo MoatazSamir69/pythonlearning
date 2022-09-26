19d71efb3c5160fd08b8226b6b60a7c579b20586
MoatazSamir69 <motaz.samir935@gmail.com>
2022-09-26 09:36:05 +0200

    Logical Operation Section in Python Learning

---
+++ Logical Operation .py
@@ -0,0 +1,13 @@
+temp = int(input("What temp? "))
+
+if temp >= 0 and temp <= 30 :
+    print("good hf")
+
+elif temp < 0 or temp >30:
+    print("Baaaaaaaaaaaad stay in Home")    
+
+#if not(temp >= 0 and temp <= 30 ):
+#    print("good hf")
+
+#elif not(temp < 0 or temp >30):
+#    print("Baaaaaaaaaaaad stay in Home") 
