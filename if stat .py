0b7d8155edc620b052e026b99babab38459c319f
MoatazSamir69 <motaz.samir935@gmail.com>
2022-09-26 09:35:21 +0200

    If stat Section in Python Learning

---
+++ if stat .py
@@ -0,0 +1,10 @@
+age = int(input("How old are you? "))
+
+if age == 100 :
+    print("Dead Man")
+elif age >= 18:
+    print("You are an adult")
+elif age <0 :
+    print("Sperm")
+else:
+    print("abo 4a5a")   
