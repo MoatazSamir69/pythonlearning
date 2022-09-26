9f04dca56507739f7777359636a5ec24933f54c4
MoatazSamir69 <114271675+MoatazSamir69@users.noreply.github.com>
2022-09-26 08:29:42 +0200

    Delete User Input .py

--- User Input .py
+++
@@ -1,8 +0,0 @@
-name = input("Whats is your name ? ") 
-age = int(input("How old are you? "))
-height = float(input("How tall are you? "))
-age = age + 1
-
-print("Hello " + name)
-print("You are " +str(age) + " Years Old")
-print("Your are " + str(height) + " cm")
